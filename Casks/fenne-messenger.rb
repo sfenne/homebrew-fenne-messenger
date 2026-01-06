da83a48da382af3d225cb877cb6fa57b93bde963
Stephen Fenne <sfenne@gmail.com>
2026-01-06 06:59:15 +0100

    v1.0.1

--- Casks/fenne-messenger.rb
+++ Casks/fenne-messenger.rb
@@ -1,6 +1,6 @@
 cask "fenne-messenger" do
   version "1.0.0"
-  sha256 "55fdb066e0eb3c2a527379558c56e68a4179a98a11df1773e1694f22ea869946"
+  sha256 "9ab2859f5b54dc8a7f2bc33d47d01f4a73d0ea0cf39f5b863abb3aa8006eef71"
   url "https://github.com/sfenne/homebrew-fenne-messenger/releases/download/v#{version}/Fenne-Messenger-mac-universal.dmg"
   name "Fenne Messenger"
   desc "A simple and minimalistic Messenger Wrapper client with nifty features."
